c41e10be8179dab6ef8110ec5248b4610b8bc361
Alfredo Lajara <alfredo_lajara@outlook.es>
2022-09-19 09:56:44 +0200

    Agregar archivos de proyecto.

---
+++ ConsolePrueba/Calendario.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsolePrueba
+{
+    class Calendario
+
+    {
+        public static void Main(string[] args)
+
+        {
+            DateTime calendario = new DateTime();
+
+        }
+    }
+}
